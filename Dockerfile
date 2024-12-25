bff3e0d1576e903aec1232b336d57fa407e9146a
Natix1 <natix1.spam@gmail.com>
2024-12-25 09:42:55 +0100

    Fixed output, now using normal ubuntu image

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ WORKDIR /app
 COPY ./src/app.py /app/
 COPY ./requirements.txt /app/
 
-RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["python", "app.py"]
+RUN apt-get update
+RUN apt-get install -y python3 python3-pip python3-venv
+RUN python3 -m venv venv
+RUN ./venv/bin/python -m pip install --no-cache-dir -r requirements.txt
+
+CMD ["./venv/bin/python", "app.py"]
--- src/app.py
+++ src/app.py
@@ -1,8 +1,15 @@
+from random import randint
+from rich import print
 from rich.console import Console
-from rich.text import Text
+from rich.highlighter import Highlighter
 
-console = Console()
+console = Console(force_terminal=True)
 
-rainbow_text = Text.from_markup("[rainbow]Hello from rainbowdockerized python!!!![/rainbow]")
+class RainbowHighlighter(Highlighter):
+    def highlight(self, text):
+        for index in range(len(text)):
+            text.stylize(f"color({randint(16, 255)})", index, index + 1)
 
-console.print(rainbow_text)
+rainbow = RainbowHighlighter()
+
+console.print(rainbow("Hello from rainbowdockerized python!"))
